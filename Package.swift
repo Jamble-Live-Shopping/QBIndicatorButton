e17a01fcf783c0696e09fc5bae15c9cb72343ab2
Gonzalo Vizeu <gonzalo@jambleapp.com>
2024-12-20 15:20:56 +0000

    add supported version

--- Package.swift
+++ Package.swift
@@ -1,17 +1,23 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.5
+
 import PackageDescription
 
 let package = Package(
     name: "QBIndicatorButton",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v12),
+    ],
     products: [
         .library(
             name: "QBIndicatorButton",
-            targets: ["QBIndicatorButton"]),
+            targets: ["QBIndicatorButton"]
+        ),
     ],
     targets: [
         .target(
             name: "QBIndicatorButton",
             path: "QBIndicatorButton/Classes"
-            )
+        )
     ]
 )

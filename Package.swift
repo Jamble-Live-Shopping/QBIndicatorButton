e99e43024d8b63b67075cc99fc6f6ca248fb4962
Gonzalo Vizeu <gonzalo@jambleapp.com>
2024-12-20 13:59:45 +0000

    add SPM

---
+++ Package.swift
@@ -0,0 +1,27 @@
+
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "QBIndicatorButton",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "QBIndicatorButton",
+            targets: ["QBIndicatorButton"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "QBIndicatorButton",
+            path: "QBIndicatorButton/Classes",
+            swiftSettings: [
+                .define("SWIFT_PACKAGE")
+            ]
+        )
+    ]
+)
